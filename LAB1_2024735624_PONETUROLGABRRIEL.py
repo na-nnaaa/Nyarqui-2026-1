7d772ea06ba27559b6b1c19d532d805adca843b4
na-nnaaa <jugailoloye@gmail.com>
2026-03-14 21:02:46 -0300

    interfaz de usuario

--- LAB1_2024735624_PONETUROLGABRRIEL.py
+++ LAB1_2024735624_PONETUROLGABRRIEL.py
@@ -1,15 +1,17 @@
-'''FALTA IMPLEMENTAR:
+'''
+FALTA IMPLEMENTAR:
 
     comentar funciones
     procesar_numero (basicamente la funcion que transforma un numero de una base a otra, 4 secciones, una para cada representacion)
     crear e integrar una funcion que maneje lo de si el numero total se pasa del rango decimal permitido, [32, 126], yo creo q puede ir en el mismo procesar_numero)
 
-    ocupa snake_case papu q es convencion internacional pa python xdd
-    pone tu rol en el nombre del archivo
+    -ocupa snake_case papu q es convencion internacional pa python xdd
+    -pone tu rol en el nombre del archivo
 
     y quiero ver el caso borde de si aparece un numero que se escapa de la base, se considera como un simbolo basura nomas? voy a preguntar en el foro
 
 '''
+
 def es_digito_valido (digito, base):
     if base == 2 and digito in list(range(0, 1, 1)): return True
 
@@ -20,11 +22,25 @@ def es_digito_valido (digito, base):
     elif base == 16 and digito in list(range(0, 15, 1)): return True
 
 
+print("--- DECODIFICADOR DE NOTAS ---")
+while True:
+    base_objetivo = int(input("Ingrese la base en la que desea visualizar los datos (2, 8, 10, 16): "))
+    if base_objetivo not in [2, 8, 10, 16]:
+        print("Por favor introduzca una base válida")
+    else: break
+
+print("[+] Procesando archivo: notas_dm.txt...")
+print("[-] Filtrando ruido místico (valores fuera de rango ASCII)...")
+
+print(f"LISTA DE VALORES EXTRAÍDOS (Base {base_objetivo}):")
+print("--------------------------------------------------")
+
 with open("notas_dm.txt", "r") as arch:
     while True:
         caracter = archivo.read(1)
         if not caracter:
             break #Fin del archivo
+
         # 1- Prefijo?
         if caracter in ["*", "&", "#", "!"]:
             if acumulador:
